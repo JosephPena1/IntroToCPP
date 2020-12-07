95128f9d604b82d6f82516c3f998fc151f88b1a2
Joseph Pena <jgpena2888@gmail.com>
2020-12-07 16:42:28 -0600

    Made function NumberGuessing;

--- IntroToCPP/Main.cpp
+++ IntroToCPP/Main.cpp
@@ -1,23 +1,55 @@
 #include <iostream>
 
-bool BinarySearch()
+bool GuessingGame()
 {
+	int num[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
 	int min = 0;
-	int max = num.length - 1;
+	int max = sizeof(num)/sizeof(*num);
 	while (min <= max)
 	{
 		int guess = ((min + max) / 2);
 
-		if (num[guess] == goal)
-			return guess;
+		//Computer's guess
+		system("cls");
+		std::cout << "Is this your number?: " << guess << std::endl;
+		//Was it correct?
+		int answer = 0;
+		std::cout << "[1] Yes" << std::endl;
+		std::cout << "[2] No" << std::endl;
+		std::cout << "> ";
+		std::cin >> answer;
 
-		else if (num[guess] < goal)
-			min = guess + 1;
+		//if yes end game. if no, Higher or lower
+		switch (answer)
+		{
+		case 1:
+			std::cout << "(^*.*^) ";
+			return true;
+			break;
+		
+		case 2:
+			system("cls");
+			int highLow = 0;
+			std::cout << "Is it Higher or Lower?" << std::endl;
+			std::cout << "[1] Higher" << std::endl;
+			std::cout << "[2] Lower" << std::endl;
+			std::cout << "> ";
+			std::cin >> highLow;
 
-		else if (num[guess] > goal)
-			max = guess - 1;
+			switch (highLow)
+			{
+			case 1:
+				min = guess + 1;
+				break;
+
+			case 2:
+				max = guess - 1;
+				break;
+			}
+			break;
+		}
 	}
-	return false;
 }
 
 void FizzBuzz()
@@ -43,14 +75,29 @@ void FizzBuzz()
 
 int main()
 {
-	
+	std::cout << "Think of a number from 1-10, then ";
 	system("pause");
+	system("cls");
+
+	GuessingGame();
+
+	system("pause");
+	system("cls");
+
 	return 0;
 
 	/*bool gameOver = false;
 
 	while (!gameOver)
 	{
+	std::cout << "Think of a number from 1-10, then ";
 
+	system("pause");
+	system("cls");
+
+	GuessingGame();
+
+	system("pause");
+	system("cls");
 	}*/
 }
